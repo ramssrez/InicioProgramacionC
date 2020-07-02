dafff81ce57752f71f41ce6174ed57e1f5657cb0
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-02 14:16:34 -0500

    Ejercicio 1, bloque tres

---
+++ DevCProjectos/ModuloTres/Cin.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+	int n1,n2;
+	cout <<"Digite dos numeros: ";
+	cin >>n1>>n2;
+	cout <<"Los numeros son: " <<n1 << " y " <<n2<<endl;
+	return 0;
+		
+}
