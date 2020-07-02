c9a56ad747ad9e139bf28a4eff2b005709fde71c
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-02 12:49:10 -0500

    Inicio del modulo tres

---
+++ DevCProjectos/ModuloTres/Sin Nombre1.cpp
@@ -0,0 +1,16 @@
+/*Condicionales if
+  if(condicion){
+     instruccion 1;
+   }else{
+     instrucción 2;
+   }
+*/
+#include <iostream>
+using namespace std;
+
+int main (){
+	int numero, dato = 5;
+	cout << "";
+	return 0;	
+}
+
