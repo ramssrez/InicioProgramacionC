dc57e97548786b1c93feb7b9819cf3baa520f462
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-03 11:41:16 -0500

    Validación del ejercicio siete, el anterior fue el seis

--- DevCProjectos/ModuloTres/EjercicioSiete.cpp
+++ DevCProjectos/ModuloTres/EjercicioSiete.cpp
@@ -6,12 +6,12 @@ indique en la salida estándar si la edad esta en el rango de 18 a 25
 using namespace std;
 int main (){
 	int edad;
-	cout << "Ingresa su edad";
+	cout << "Ingresa su edad: ";
 	cin>> edad;
 	if((edad >= 18)&&(edad <= 25)){
 		cout <<"Estas en el rango de edad de 18 a 25";
 	}else {
-		cout <<"No estas en el rango de edad";
+		cout <<"No estas en el rango de edad entre 18 a 25";
 	}
 	return 0;
 }
