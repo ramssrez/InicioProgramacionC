aef75f5216dbd5df8115abe302e2e763625b90e3
Raul Ramirez Perez <ramssrez@gmail.com>
2020-07-03 12:56:13 -0500

    Finalización del ejercicio con las diferentes opciones

--- DevCProjectos/ModuloTres/EjercicioNueve.cpp
+++ DevCProjectos/ModuloTres/EjercicioNueve.cpp
@@ -15,6 +15,7 @@ int main(){
 	cout <<"Ingresa el numero entero: ";
 	cin >>numero;
 	//Primero se tiene que dividir el numeoro para obtener sus miles, las centenas, las decenas y las unidades
+	//De est aforma obtenemos los diferentes nuero que lo componen
 	unidades = numero%10;
 	numero /=10;
 	cout<<"Unidades "<<unidades<<endl;
@@ -31,5 +32,44 @@ int main(){
 	numero /=10;
 	cout<<"Miles "<<miles<<endl;
 	cout <<numero<<endl;
+	
+	switch(miles){
+		case 1: cout<<"M";break;
+		case 2: cout<<"MM";break;
+		case 3: cout<<"MMM";break;
+	}
+	switch(centenas){
+		case 1: cout<<"C";break;
+		case 2: cout<<"CC";break;
+		case 3: cout<<"CCC";break;
+		case 4: cout<<"CD";break;
+		case 5: cout<<"D";break;
+		case 6: cout<<"DC";break;
+		case 7: cout<<"DCC";break;
+		case 8: cout<<"DCCC";break;
+		case 9: cout<<"CM";break;
+	}
+	switch(decenas){
+		case 1: cout<<"X";break;
+		case 2: cout<<"XX";break;
+		case 3: cout<<"XXX";break;
+		case 4: cout<<"XL";break;
+		case 5: cout<<"L";break;
+		case 6: cout<<"LX";break;
+		case 7: cout<<"LXX";break;
+		case 8: cout<<"LXXX";break;
+		case 9: cout<<"XC";break;
+	}
+	switch(unidades){
+		case 1: cout<<"I";break;
+		case 2: cout<<"II";break;
+		case 3: cout<<"III";break;
+		case 4: cout<<"IV";break;
+		case 5: cout<<"V";break;
+		case 6: cout<<"VI";break;
+		case 7: cout<<"VII";break;
+		case 8: cout<<"VIII";break;
+		case 9: cout<<"IX";break;
+	}
 	return 0;
 }
