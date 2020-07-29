168777bb192e7fbad0e3b1cd1410766270342cce
ramssrez <ramssrez@gmail.com>
2020-07-29 12:53:01 -0500

    modificacion de los valores de salida en c

--- carreraUnadm/CicuferenciaC.c
+++ carreraUnadm/CicuferenciaC.c
@@ -2,17 +2,21 @@
 biblioteca o directivas del procesador
 */
 #include <stdio.h>
+#include<math.h>
 //Declaración global
 const float PI=3.1416;
 //Funcion principal main
 main(){
 	//Declaración de variables locales de la funcion main
-	float radio, area;
-	printf("Calcula el área de una circuferencia \n");
+	float radio, area, areaDos;
+	int radioDos = 3;
+	printf("Calcula el area de una circuferencia \n");
 	printf("Ingrese el radio: ");
 	scanf("%f",&radio);
 	area = PI * radio * radio;
-	printf("Area = %f",area);	
+	areaDos=PI*pow(radio,2);
+	printf("Area = %f \n",area);
+	printf ("El area del circulo de radio: %d es %.2f \n",radioDos, area);	
 	getchar();
 	getchar();
 }
