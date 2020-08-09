120a5ac544a98af99bad97daf59110457175c1b2
ramssrez <ramssrez@gmail.com>
2020-08-09 16:36:07 -0500

    Ejercicio 1 fin

--- U2_A2_RARP.c
+++ U2_A2_RARP.c
@@ -14,15 +14,11 @@ mensaje indicando que la tienda ya va a cerrar.
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <windows.h>
 int main(void){
-	int numeroPersona=0,personaEntra=0,personaSale=0, contador, segundos=0, minutos=0,horas=0,personaTotal=0,total=0;
-//	printf("Ingresa el numero de personas que entran a la tienda: ");
-//	scanf("%d",&personaEntra);
-//	printf("ingresa el numero de personas que salen de la tienda ");
-//	scanf("%d",&personaSale);
-	//printf("El numero de personas son: %d \n",personaSale);
+	//int contador, segundos=0, minutos=0,horas=0,personaTotal=0,total=0;
+	unsigned int numeroPersona=0,personaEntra=0,personaSale=0,personaTotal=0,total=0; 
+	printf("*********** Bienvenidos a nuestra tienda departamenta  *********** \n\n\n");
+	printf("*********** Por disposción oficial solo podemos ingresar un límite de persona *********** \n\n\n");
 	
 	do {
 		printf("Ingresa el numero de personas que entran a la tienda: ");
@@ -30,31 +26,25 @@ int main(void){
 		printf("ingresa el numero de personas que salen de la tienda ");
 		scanf("%d",&personaSale);
 		numeroPersona =numeroPersona + personaEntra;
-		//totalEntra = totalEntra +personaEntra;
 		personaTotal = personaEntra-personaSale;
 		total = total + personaTotal;
-		printf("En la tienda hay: %.2d\n", total);
-		printf("En la tienda han ingresado: %.2d\n", numeroPersona);
-		
-		if(numeroPersona==4){
-			printf("El limite es excedido \n");
+		if(total<=0){
+			total =0;
 		}
-
+		printf("En la tienda han ingresado: %d", numeroPersona);
+		printf(" personas \n");
+		printf("En la tienda hay: %d", total);
+		printf(" personas \n\n");
 		
+		if (total >= 40){
+			printf("*********** Solo pueden salir e ingresar de a dos personas cada 10 minutos ***********\n\n\n");
+		}
 		
-	}while(numeroPersona<=10);
-	
-	
-	
-//	while(1){
-//		segundos++;
-//		printf("%.2d \n",segundos);
-//		Sleep(1000);
-//		if (segundos==60){
-//			break;
-//		}
-//	}
-	//printf("Los segundos son %.2d \n ",segundos);
+		if((total==0)||(numeroPersona>=100)){
+			break;
+		}
+	}while(1);
+	printf("*********** La tienda esta cerrada agradecemos tu compra ***********\n");
 	system("pause");
 	return 0;
 }
