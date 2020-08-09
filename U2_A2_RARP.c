9efcf5602dfc79a45e3081e4ecc6adfdbb97781e
ramssrez <ramssrez@gmail.com>
2020-08-09 16:44:38 -0500

    fin del ejercicio

--- U2_A2_RARP.c
+++ U2_A2_RARP.c
@@ -14,35 +14,43 @@ mensaje indicando que la tienda ya va a cerrar.
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <wchar.h>
+#include <locale.h>
 int main(void){
 	//int contador, segundos=0, minutos=0,horas=0,personaTotal=0,total=0;
+	setlocale(LC_ALL, "");
 	unsigned int numeroPersona=0,personaEntra=0,personaSale=0,personaTotal=0,total=0; 
-	printf("*********** Bienvenidos a nuestra tienda departamenta  *********** \n\n\n");
-	printf("*********** Por disposción oficial solo podemos ingresar un límite de persona *********** \n\n\n");
+	printf("********************************* ¡Bienvenidos a nuestra tienda departamental!  ********************************* \n\n\n");
+	printf("*********** Por disposición oficial solo podemos ingresar un límite de persona *********** \n\n\n");
 	
 	do {
-		printf("Ingresa el numero de personas que entran a la tienda: ");
+		printf("Ingresa el número de personas que entran a la tienda: ");
 		scanf("%d",&personaEntra);
-		printf("ingresa el numero de personas que salen de la tienda ");
+		printf("ingresa el número de personas que salen de la tienda ");
 		scanf("%d",&personaSale);
 		numeroPersona =numeroPersona + personaEntra;
 		personaTotal = personaEntra-personaSale;
 		total = total + personaTotal;
+		
 		if(total<=0){
 			total =0;
 		}
+		
 		printf("En la tienda han ingresado: %d", numeroPersona);
 		printf(" personas \n");
 		printf("En la tienda hay: %d", total);
 		printf(" personas \n\n");
 		
 		if (total >= 40){
-			printf("*********** Solo pueden salir e ingresar de a dos personas cada 10 minutos ***********\n\n\n");
+			if(personaTotal<100){
+				printf("*********** Solo pueden salir e ingresar de a dos personas cada 10 minutos ***********\n\n\n");
+			}
 		}
 		
 		if((total==0)||(numeroPersona>=100)){
 			break;
 		}
+		
 	}while(1);
 	printf("*********** La tienda esta cerrada agradecemos tu compra ***********\n");
 	system("pause");
