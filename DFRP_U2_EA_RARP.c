b8ccbfc2487c72dcd20d0d6b4c790484b26d40b2
ramssrez <ramssrez@gmail.com>
2020-08-10 21:32:35 -0500

    vector de calificaciones

--- DFRP_U2_EA_RARP.c
+++ DFRP_U2_EA_RARP.c
@@ -1,13 +1,41 @@
+/*
+Una Universidad debido a la pandemia actual del COVID-19 ha decido que sus profesores
+registren las calificaciones de sus alumnos que realizaron examen extraordinario por medio
+de un sistema informático, debido a las circunstancias se requiere elabora un programa en
+C que contenga un menú con 3 opciones:
+1. Capturar Exámenes
+2. Mostrar Indicadores
+3. Salir.
+
+El programa deberá continuar hasta que el profesor proporcione la opción 3. Salir.
+Descripción:
+3.1. La opción 1. Capturar Exámenes
+3.1.1. El profesor evaluó N alumnos que realizaron 3 exámenes y para determinar su
+aprovechamiento académico requiere capturar las calificaciones de los 3 exámenes
+de los alumnos evaluados.
+3.2. Opción 2. Mostrar Indicadores
+3.2.1. A partir de las calificaciones capturadas en la Opción 1, se deberá mostrar en
+pantalla los siguientes indicadores:
+? Número de alumnos que aprobaron todos los exámenes
+? Número de alumnos que aprobaron por lo menos un examen.
+? Número de alumnos que aprobaron únicamente el último examen.
+3.3. Opción 3. Salir
+*/
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
-int op,op1,op2,i=0,calif,suma;
+//Librerías que nos permiten agregar caracteres especiales al texto de salida
+#include <wchar.h>
+#include <locale.h>
+int op,i,calif,calif1,calif2,suma;
+int calificacion [10];
 int main(){
+	setlocale(LC_ALL, "");
+
     printf("\n\t\tBienvenido al sistema de calificaciones\n\n\t");
  
     do
     {
-    	printf("\n\tSeleccione la opcion de su preferencia \n\n\t");
+    	printf("\n\tSeleccione la opción de su preferencia \n\n\t");
         printf("\n\t1)Ingresar las calificaciones \n\t2)Mostrar los indicadores \n\t3)Salir del sistema de calificaciones.\n\t");
         scanf("%i",&op);
 
@@ -17,13 +45,20 @@ int main(){
                                             			  
   							for(i=1;i<4;i++)
 							  {
-							    printf("\n Escriba la calificacion numero %d \n",i);
-							    printf("\t");
-								scanf("%d,%d,%d",&calif);
+							    printf("\n Escriba la calificacion numero %d :\n",i);
+							//	scanf("%d,%d,%d",&calif,&calif1,&calif2);
+								scanf("%d", &calificacion[i]);
 							   }
-							 printf("\n\n Las calificaciones se han guardado \n\n\t",i);
+							 printf("\n\n Las calificaciones se han guardado \n\n",i);
+							 
+							
+							 for (i=1;i<4;i++){
+							 	printf("Calificación %d :  %d \n",i, calificacion[i]);
+							 }
+							 
 							 printf("\n\n Ha regresado al Menu \n\n\t",i);
 							 
+
 							 printf("\n\n ------------------------------------------------------------------------------------------------------ \n\n",i);
 							
                   
@@ -54,6 +89,8 @@ int main(){
     printf("\n\tAcaba de salir del programa de compra de Videojuegos\n\t");
 
 
-    getch();
+
+
+    system("pause");
     return 0;
 }
