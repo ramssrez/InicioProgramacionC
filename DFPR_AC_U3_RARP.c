3f5e0c10f33b65c3fc55d20765a250e2d42aa910
ramssrez <ramssrez@gmail.com>
2020-08-26 12:01:00 -0500

    Funciones de ingresar matriz y mostrarla

--- DFPR_AC_U3_RARP.c
+++ DFPR_AC_U3_RARP.c
@@ -3,13 +3,20 @@
 //Librerías que nos permiten agregar caracteres especiales al texto de salida
 #include <wchar.h>
 #include <locale.h>
-int opcion;
+int unsigned i,j;
+int orden=0, opcion;
+int matriz [100][100];
+void registrarMatriz();
+void imprimirMatriz();
+void validarOrden();
+int valorMaximoMatriz();
 void imprimirFinOpcion();
 void imprimirInicioOpcion();
 int main(){
 	//Método que nos permite el ingreso de caracteres especiales como los acentos
 	setlocale(LC_ALL, "");
 	//Declaración de variables locales.
+	int valorMayor;
 	//int opcion;
 	//Presentación del programa
 	printf(" ----------------------------------------------------------------------------------------------------------------- ");
@@ -29,22 +36,27 @@ int main(){
         switch(opcion){
         	case 1:
         		imprimirInicioOpcion(opcion);
-        		printf("Caso 1");//Aqui va la función a realizar
+        		registrarMatriz();
         	    imprimirFinOpcion();
         		break;
         	case 2:
         		imprimirInicioOpcion(opcion);
-        		printf("Caso 2");
+        		validarOrden();
         		imprimirFinOpcion();
         		break;
         	case 3:
         		imprimirInicioOpcion(opcion);
-        		printf("Caso 3");
+        		valorMayor = valorMaximoMatriz();
+        		printf("El valor máximo de la matriz es: %d",valorMayor);
         		imprimirFinOpcion();
 				break;
         	case 4:
         		imprimirInicioOpcion(opcion);
         		printf("\n\t Usted acaba de salir del menú\n");
+        		printf("\n\n **************************** Agradecemos si visita, vuelva pronto ****************************\n\n\t");
+    			printf("\n\n ------------------------------------------------------------------------------------------------------ \n\n");
+				system("pause");
+				return 0;
         		break;
         	default:
         		imprimirInicioOpcion(opcion);
@@ -56,10 +68,44 @@ int main(){
 		}
 		
 	}while(opcion!=4);
-	printf("\n\n **************************** Agradecemos si visita, vuelva pronto ****************************\n\n\t");
-    printf("\n\n ------------------------------------------------------------------------------------------------------ \n\n");
-	system("pause");
-	return 0;
+}
+void registrarMatriz(){
+	printf("Ingresa el orden de la matriz: ");
+	scanf("%d",&orden);
+	if (orden<=1){
+		printf("\tEl orden no es el adecuado, debe ser mayor a 2");
+	}else{
+		for (i = 0; i<orden;i++){
+			for (j =0; j<orden;j++){
+				printf("\t");
+				printf("Elemento [%d,%d]: ", i,j);
+				scanf("%d",&matriz[i][j]);
+			}
+		}
+	}
+
+}
+void imprimirMatriz(){
+	printf("La matriz es la siguiente: \n");
+	for (i = 0; i<orden;i++){
+		for (j =0; j<orden;j++){
+			printf("\t");
+			printf("%d",matriz[i][j]);
+		}
+		printf("\n");
+	}
+}
+void validarOrden(){
+	if(orden<=1){
+		printf("No se han registrado valores, vuelve al menú principal y elige la primera opción \n");
+	}else{
+		imprimirMatriz();
+	}
+}
+int valorMaximoMatriz(){
+	int mayor;
+	mayor = 5;
+	return mayor;
 }
 void imprimirFinOpcion(){
 	printf("\n\n **************************** Ha regresado al menú principal ****************************\n\n\t");
@@ -84,6 +130,4 @@ void imprimirInicioOpcion(int opcion){
 			printf("\n\t Ha elegido una opción invalida, vuelva a escoger una opción\n");
 			break;
 	}
-	
-
 }
