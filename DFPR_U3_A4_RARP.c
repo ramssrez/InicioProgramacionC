dbf34121ec6139e1a25451a1925438f75dad1132
ramssrez <ramssrez@gmail.com>
2020-08-29 17:47:37 -0500

    inicio de las pilas en c

--- DFPR_U3_A4_RARP.c
+++ DFPR_U3_A4_RARP.c
@@ -1,44 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-//https://es.wikibooks.org/wiki/Programaci%C3%B3n_en_C/Algoritmos_y_Estructuras_de_Datos
 
-int main()
-{
-   srand(time(NULL));
-   unsigned int i=0,j=0,variable,A[12];
-  
-   printf( "Estos son 10 numeros aleatorios: \n\n" );
-   for( i=0; i<10; i++ ){
-   	 A[i]= rand()%20;
-   }
-    for( i=0; i<10; i++ ){
-    printf( "%d,", A[i] );
-	}
-	
-	for(i=0; i<10; i++ ){
-		for( j=0; j<10; j++ ){
-			if(A[j]>A[j+1])
-			{
-				variable=A[j];
-				A[j]=A[j+1];
-				A[j+1]=variable;
-				
-			}
-		}
-	}
-	printf( "\nLos numeros ordenados son:" );
-			
-	for( i=0; i<10; i++ ){
-    printf( "\%d,", A[i] );
-	}
-	
-	printf( "\nEl numero maximo es:%d", A[9]);
+#define true 1
+#define false 0
+
+struct {
+	char nombre[20];
+	int id;
+	char apellido[20];
 		
-	
-  
-  return 0;  
+}persona;
+int main(){
+	printf("Hola mundo desde C");
+	return 0;
 }
-   
-   
-
