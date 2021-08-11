c3768fb9caf59ff935730ce4624af0fa81719d97
ramssrez <ramssrez@gmail.com>
2021-08-10 20:25:35 -0500

    fianlización de la busqueda binaria

--- B_Lineal_A2_RARP.cpp
+++ B_Lineal_A2_RARP.cpp
@@ -5,41 +5,80 @@
 #include <time.h>
 
 using namespace std;
-int arreglo[9], aleatorio, valorArreglo=-1;
+int arreglo[9], aleatorio=-1, valorArreglo=-1, iteraciones=0;
+bool bandera = false;
 
 void numeroAleatorio(){
-	srand(time(NULL));
-	aleatorio = 1 + rand() % (101-0);
+	cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
+	cout << "Elegiste la opción 2" <<endl;
+	aleatorio = 1 + rand() % (100 - 0);
+	cout<< "Se ha creado un número aleatorio entre el 0 al 100" <<endl;
+	cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
 }
 
+void busquedaSecuencial(){
+	for(int i = 0; i<10; i++){
+		if(arreglo[i] == aleatorio){
+			iteraciones = (i+1);
+			bandera = true;
+			break;
+			}
+		}		
+}	
+
 void impresionArreglo(){
-	if(valorArreglo == -1){
-		cout<<"El arreglo esta vacío, selecciona la opción 1";
-	}else{
-		cout << "[ ";
-		for(int i = 0; i<10; i++){
-			cout <<arreglo[i] << " ";
-		}
-		cout << "] " <<endl;
+	cout << "[ ";
+	for(int i = 0; i<10; i++){
+		cout <<arreglo[i] << " ";
 	}
+	cout << "] " <<endl;	
 }
 
 void arregloAleatorio(){
+	cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
+	cout << "Elegiste la opción 1" <<endl;
 	int num; 
-	srand(time(NULL));
     for(int i = 0; i < 10; i++)
     {
-        num = 1 + rand() % (101 - 0);
+        num = 1 + rand() % (100 - 0);
         arreglo[i] = num;
-        cout <<arreglo[i] << " ";
     }
     valorArreglo = 9;
+    cout<< "Se ha creado un arreglo con números aleatorios del 0 al 100" <<endl;
+    cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
+}
+
+void validacionBusqueda(){
+	cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
+	cout << "Elegiste la opción 3" <<endl <<endl;
+	if(valorArreglo == -1){
+		cout <<"No se han generado números en el arreglo, selecciona la opción 1" <<endl;
+		cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
+	}else if(aleatorio == -1){
+		cout<<"No se ha generado un número aleatorio, selecciona la opción 2" <<endl;
+		cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
+	}else{
+		cout <<"El arreglo es: " <<endl;
+		impresionArreglo();
+		cout<< " " <<endl;
+		cout << "El número aleatorio es: "  <<aleatorio <<endl;
+		busquedaSecuencial();
+		if(bandera){
+			cout << "Se ha encontrado el número " <<aleatorio <<" con un número de iteraciones igual: " << iteraciones <<endl;
+			bandera = false; 
+			iteraciones = 0;
+		}else{
+			cout <<"No se encuentra el número " << aleatorio << " dentro del arreglo" << endl;;
+		}
+		cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
+	}
+	
 }
 
 int main(){
 	int opcion;
     bool repetir = true;
-    
+    srand(time(NULL));
     
     do {
     	setlocale(LC_ALL, "");
@@ -50,10 +89,6 @@ int main(){
         cout << "2. Generación de número aleatorio del 0 al 100" << endl;
         cout << "3. Mostrar arreglo y número a buscar" << endl;
         cout << "4. Salir" << endl;
-        cout << "5. Mostrar los elementos de la Pila" << endl;
-        cout << "6. Medir el tamaño de la Pila" <<endl;
-        cout << "7. Generar un Pila con números aleatorios" <<endl;
-        cout << "8. SALIR" << endl;
          cout << "\nIngrese una opcion: ";
         cin >> opcion;
         
@@ -63,13 +98,11 @@ int main(){
                 break;
                 
             case 2:
-            	//validacionLlena();
             	numeroAleatorio();
-            	cout<<aleatorio;
                 break;
                 
             case 3:
-            	impresionArreglo();
+            	validacionBusqueda();
                 break;
                 
             case 4:                            
@@ -78,31 +111,7 @@ int main(){
             	cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
                	repetir = false;                           
                 break;
-            
-            case 5:
-                //mostrarPila();
-            	break;
-            	
-            case 6:
-            	cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
-				//cout<< "La Pila solo se pueden ingresar 10 elementos, en este momento el tamaño de la Pila es: " <<(tope + 1) <<endl;
-				cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
-            	break;
-            	
-            case 7:
-            	cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
-            	cout << "Se ha creado una Pila con números aleatorios entre el 0 al 100, selecciona la opción 5 para observar la Pila." <<endl;
-            	//pilaAleatorio();
-            	cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
-            	break;
-            	
-            case 8:
-            	cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
-            	cout << "Gracias por usar este programa." <<endl;
-            	cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
-               	repetir = false;
-            	break;
-            	
+                        	
             default:
             	cout<<"--------------------------------------------------------------------------------------------------------------------" <<endl;
             	cout <<"Esta no es una opción del menú" <<endl;
